1dc8e59903a0dcfa57b3b4f92b4b2e979a1e1d8f
DavidPark9830 <davidpark9830@gmail.com>
2026-09-16 19:43:40 +0900

    Use balanced Piper voice model

--- archive/phd_prep/gre/verbal/voca/tts-source.js
+++ archive/phd_prep/gre/verbal/voca/tts-source.js
@@ -1,6 +1,6 @@
 import { TtsSession, stored } from "@mintplex-labs/piper-tts-web";
 
-export const VOICE_ID = "en_US-lessac-high";
+export const VOICE_ID = "en_US-lessac-medium";
 
 export async function isVoiceCached() {
   try {
